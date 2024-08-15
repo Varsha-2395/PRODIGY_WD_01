f57c50ddb2657b8bc90fb9ec75333a8a7748f8eb
Varsha <167612098+Varsha-2395@users.noreply.github.com>
2024-08-15 15:46:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
